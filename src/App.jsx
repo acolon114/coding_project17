d19b88ffe0c675f13a9d48528c5851ecc577d405
acolon114 <acolon114@usf.edu>
2024-11-25 21:11:05 -0900

    Updated Comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,8 @@
 //Task 1: App.jsx (Root Component)
 import React from "react";
-//import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Gallery from "../components.jsx/Gallery.jsx"; // Import the Gallery Component
-// Toolbar from "../components.jsx/Toolbar.jsx"; 
-import "./App.css"; // Global styling
+import Gallery from "../components.jsx/Gallery.jsx"; 
+import "./App.css"; 
 
-//const App = () => {
-//   return (
-//     <Router>
-//       <div className="app">
-//         {/* Toolbar or Header */}
-//         <Toolbar title="Tour Gallery" />
-
-//         {/* Application Routes */}
-//         <Routes>
-//           {/* Main route rendering the Gallery component */}
-//           <Route path="/" element={<Gallery />} />
-//           {/* Additional routes can be added here if needed */}
-//         </Routes>
-//       </div>
-//     </Router>
-//   );
-// };
 function App() {
   
   return (
